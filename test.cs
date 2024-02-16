76dd57ed7135dd39c61fe5518ce28078304ebf22
Gabin Lee <gb94@ewhain.net>
2024-02-16 13:36:38 +0900

    아이템타입 랜덤 함수

--- test.cs
+++ test.cs
@@ -7,7 +7,7 @@ using Items;
 
 public class MapManager : MonoBehaviour
 {
-    public string[] map = new string[10];
+    public string[] map = new string[13];
     string newPlace;
     int i = 0;
 
@@ -15,11 +15,11 @@ public class MapManager : MonoBehaviour
     private string CallGPT()
     {
         // GPT 호출 코드~~
-        newPlace = "GPT가 만든 장소명"
+        newPlace = "GPT가 만든 장소명";
         return newPlace;
     }
 
-    // 0번 인덱스 == PNPC 장소
+    // 0번 인덱스 == PNPC 장소 : 인트로에서 호출
     public void CreateMap()
     {
         PNPCPlace = CallGPT();
@@ -28,21 +28,49 @@ public class MapManager : MonoBehaviour
     }
 
     // map 배열에 장소 저장
-    public void AddMap()
+    for(int ch = 1, ch< 5, ch++;)
     {
-        map[i] = newPlace; //해당 칸에 gpt 호출 값 저장
+        void AddMap()
+    {
+        map[i] = "Chapter" + i;//CallGPT(); //해당 칸에 gpt 호출 값 저장
         i++;
     }
+}
 
-    // 장소 생성
-    public string GetMap(string background, string genre)
-    {
+public static Type? RandomItemType()
+{
+    // enum의 모든 값 가져오기 : Recover, Mob, Weapon
+    Array values = Enum.GetValues(typeof(Type));
+
+    // 랜덤 인덱스 생성
+    Random random = new Random();
+    int randomIndex = random.Next(values.Length + 1); // null 값 고려하여 + 1
+
+    // 랜덤값이 null인 경우 null 리턴
+    if (randomIndex == values.Length)
+        return null;
+
+    // 선택된 아이템 타입 값
+    return (Type)values.GetValue(randomIndex);
+}
+
+// 테스트 메인문
+public static void Main(string[] args)
+{
+    // 랜덤으로 선택된 enum 값 또는 null 출력
+    Console.WriteLine("랜덤으로 선택 아이템 타입 or (null): " + RandomItemType());
+}
+}
+
+// 장소 생성
+public string GetMap(string background, string genre)
+{
     private string map_setting = "너는 플레이어가 탐색할 수 있는 장소를 한 단어로 출력해야 해.";
-    private string query = background + " 배경의 " + genre + "분위기에 어울리는 장소명 1개를 출력해줘";
-    string[] messages = { map_setting, query };
-    CreateMap();
+private string query = background + " 배경의 " + genre + "분위기에 어울리는 장소명 1개를 출력해줘";
+string[] messages = { map_setting, query };
+CreateMap();
 
-        return map[i];
+return map[i];
     }
 
 // 아이템 생성
